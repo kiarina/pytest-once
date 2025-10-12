26257c0755b3cdda5089d8ac06278f225917d005
kiarina <kiarinadawa@gmail.com>
2025-10-12 23:48:08 +0900

    feat: add __version__ using importlib.metadata in __init__.py

--- pytest_once/__init__.py
+++ pytest_once/__init__.py
@@ -1,5 +1,9 @@
 """pytest-once: xdist-safe 'run once' fixture decorator for pytest."""
 
+from importlib.metadata import version
+
 from pytest_once.decorator import once_fixture
+
+__version__ = version("pytest-once")
 
 __all__ = ["once_fixture"]
